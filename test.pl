fead9ed106dcc8d9e27002739d73b2375f99cbfd
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-11-09 14:56:50 +0100

    better main script

--- test.pl
+++ test.pl
@@ -6,12 +6,41 @@ use TranslationChecker::Report;
 use TranslationChecker::Report::HTML;
 use TranslationChecker::Report::Text;
 use Data::Dumper;
+use File::Glob ':glob';
+use Sysadm::Install qw(blurt);
 
-my $lang = shift;
-my @files = @ARGV;
+sub get_available_languages {
+  my (@files) = @_;
+  @files = map { s/(\.properties$)/_\*$1/; $_ } @files;
+  @files = map { bsd_glob($_) } @files;
+  @files = map { s/.*_(\w+)\.properties$/$1/; $_ } @files;
+  my %files = map { $_ => 1 } @files;
+  @files = keys %files;
+}
 
-my @reports = map { TranslationChecker::Report::generate($_, $lang) } @files;
+sub write_reports {
+  my ($outdir, @files) = @_;
+  my @lang = get_available_languages(@files);
 
-TranslationChecker::Report::HTML::format(@reports);
+#  my %reports;
+  for my $lang (@lang) {
+    say STDERR "generating report for $lang";
 
+    my @reports = map { TranslationChecker::Report::generate($_, $lang) } @files;
+    my $html = TranslationChecker::Report::HTML::format(@reports);
+    
+    my $file = "$outdir/report_$lang.html";
+    blurt($html, $file);
+    
+    #$reports{$lang} = \@reports;
+  }
+}
 
+sub main {
+  my @files = @ARGV;
+  my $outdir = "/home/peterss/tmp/translation/html";
+
+  write_reports($outdir, @files);
+}
+
+main();
